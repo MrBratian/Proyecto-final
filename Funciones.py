06fbb09cad7923fabdaebc0612d4ebdc18e29afe
MrBratian <57512279+MrBratian@users.noreply.github.com>
2023-10-12 20:33:39 +0000

    Initial commit

---
+++ Funciones.py
@@ -0,0 +1,116 @@
+import string
+import unicodedata
+from langdetect import detect
+import nltk
+#from nltk.corpus import stopwords
+import stopwordsiso as stopwords
+
+abecedario = string.ascii_lowercase 
+
+#Función contar palabras
+def contar_palabras(contenido):
+    
+    str_contenido = ''.join(contenido)
+    palabras = str_contenido.split()
+    a=len(palabras)
+    return a
+
+#Función que lee la cantidad de caracteres que tiene el libro
+def contar_caracteres(contenido):
+     str_contenido=""
+     for linea in contenido:
+          str_contenido += linea.strip()
+        
+     a=len(str_contenido)
+     return a
+
+
+#Función que cuenta la cantidad de veces que se repite cada letra
+def calcular_frecuencia_letras(contenido,abecedario):
+    contenido = ''.join(contenido)
+    contenido = unicodedata.normalize('NFKD', contenido.casefold())
+    frecuencia_letras = {letra: 0 for letra in abecedario}
+    for letra in contenido:
+        if letra in frecuencia_letras:
+            frecuencia_letras[letra] += 1
+    a=[]
+    for letra, frecuencia in frecuencia_letras.items():
+        a.append(frecuencia)
+    return a
+
+
+#Función que muestra la frecuencia de la longitud de las palabras
+
+def contar_palabras_por_longitud(contenido):
+    str_contenido = ' '.join(contenido)  
+    palabras = str_contenido.split()
+    longitud_palabras = {}
+    for palabra in palabras:
+        longitud = len(palabra)
+        if longitud not in longitud_palabras:
+            longitud_palabras[longitud] = 1
+        else:
+            longitud_palabras[longitud] += 1
+    return longitud_palabras
+
+
+#Función que muestra las calcula la fecuncia de las palabras y muestra las 100 mas frecuentes
+def contar_frecuencia_palabras(contenido):
+    str_contenido = ''.join(contenido)
+    palabras = str_contenido.split()
+    frecuencia_palabras = {}
+    
+    # Contar la frecuencia de cada palabra
+    for palabra in palabras:
+        palabra = palabra.lower()  
+        if palabra in frecuencia_palabras:
+            frecuencia_palabras[palabra] += 1
+        else:
+            frecuencia_palabras[palabra] = 1
+    # Imprimir las 100 palabras más frecuentes
+    print("\nLas 100 palabras más frecuentes son:")
+    for palabra, frecuencia in sorted(frecuencia_palabras.items(), key=lambda x: x[1], reverse=True)[:100]:
+        print(f"{palabra}: {frecuencia} veces")
+
+#Funcion que calcula las palabras sin repetir
+def calcular_numero_distinto_palabras(contenido):
+  str_contenido = ' '.join(contenido)
+  palabras = str_contenido.split()
+  numero_distinto_palabras = len(set(palabras))
+  return numero_distinto_palabras
+
+#Funcion que detecta el idioma del texto
+def identificar_idioma(contenido):
+  str_contenido = ' '.join(contenido)
+  idioma = detect(str_contenido)
+
+  if idioma == 'en':
+    return 'English'
+  elif idioma == 'es':
+    return 'Español'
+  elif idioma == 'fr':
+    return 'Français'
+  elif idioma == 'de':
+    return 'Deutsch'
+  elif idioma == 'pt':
+    return 'Português'
+  else:
+    return 'Unknown'
+
+
+#Funcion que cuenta las palabras sin contar las stopwords
+def obtener_palabras_frecuentes_sin_stopwords(contenido, n=100):
+      idiomas = identificar_idioma(contenido).values()
+      stopwords_idioma = stopwords.load(idioma)
+      str_contenido = ' '.join(contenido)
+      palabras = str_contenido.split()
+      palabras_filtradas = [palabra.lower() for palabra in palabras if palabra.lower() not in stopwords_idioma and palabra not in string.punctuation]
+      frecuencia_palabras = {}
+      for palabra in palabras_filtradas:
+          if palabra in frecuencia_palabras:
+              frecuencia_palabras[palabra] += 1 
+          else:
+              frecuencia_palabras[palabra] = 1
+      palabras_mas_frecuentes = sorted(frecuencia_palabras.items(), key=lambda x: x[1], reverse=True)[:n]
+      return palabras_mas_frecuentes
+
---
+++ main.py
@@ -0,0 +1,109 @@
+import Funciones
+import string
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import matplotlib as mpl
+
+print("Welcome to the book analyzer")
+print("Selec your language:")
+print("1. English")
+print("2. Español")
+print("3. Français")
+print("4. Deutsch")
+print("5. Português")
+
+opcion_lenguage = int(input("Input your option: "))
+
+while True:
+    if 1<=opcion_lenguage<=5:
+        print("You have selected the option: ",opcion_lenguage)
+        break
+    else:
+        print("You have selected an invalid option")
+        opcion_lenguage = int(input("Input a valid option: "))
+abecedario = string.ascii_lowercase 
+frecuentes_por_idioma = {}
+bucle = 1
+direccion_libro = str(input("Ingrese la dirección del libro que desea analizar (para su comodidad guarde el archivo .txt en la misma carpeta del programa): "))
+try:
+    with open(direccion_libro, 'r', encoding='utf-8') as archivo:
+        contenido = archivo.readlines()
+except FileNotFoundError:
+    # Si el archivo no se encuentra, mostrar un mensaje y salir del programa
+    print(f"Error: No se encontró el archivo en la dirección especificada: {direccion_libro}")
+    bucle = 0  # Salir del bucle principal
+except Exception as e:
+    # Capturar cualquier otra excepción y mostrar un mensaje de error
+    print(f"Error inesperado al abrir el archivo: {e}")
+    bucle = 0  # Salir del bucle principal
+while bucle == 1:
+    print("\naca va la descripcion del programa")
+    # Mostrar el menú y procesar la opción del usuario
+    print("\nEl programa ofrece las siguientes herramientas:\n",
+          "1. Contar caracteres\n",
+          "2. Contar palabras\n",
+          "3. Calcular frecuencia de letras\n",
+          "4. Calcular palabras por longitud\n",
+          "5. Calcular frecuencia de palabras\n"
+          " 6. Salir")
+    opcion_herramienta = int(input("Seleccione la opción que desea realizar:"))
+
+    while True:
+        if 1 <= opcion_herramienta <= 6:
+            print("You have selected the option:", opcion_herramienta)
+            break
+        else:
+            print("You have selected an invalid option")
+            opcion_herramienta = int(input("Input a valid option: "))
+    
+    if opcion_herramienta == 1:
+        while True:
+            print(Funciones.contar_caracteres(contenido))
+    elif opcion_herramienta == 2:
+        print(Funciones.contar_palabras(contenido))
+    elif opcion_herramienta == 3:
+        print("Revisar salida")
+        print("Indicar al usuario que cierre la ventana")
+        x = list(abecedario)
+        y = Funciones.calcular_frecuencia_letras(contenido, x)
+        plt.title('Frecuencia de letras en el libro', fontsize=30)
+        plt.xlabel('Letras', fontsize=20)
+        plt.ylabel('Frecuencia', fontsize=20)
+        cmap = mpl.colormaps['RdYlGn']
+        norm = colors.Normalize(vmin=min(y), vmax=max(y))
+        histogram_abc = plt.bar(x, y, color=cmap(norm(y)))
+        for bar in histogram_abc:
+            yval = bar.get_height()
+            plt.text(bar.get_x(), yval + .005, yval)
+        plt.tight_layout()
+        plt.show()
+    elif opcion_herramienta== 4:
+        print("Revisar salida")
+        print("Indicar al usuario que cierre la ventana")
+        x=list(Funciones.contar_palabras_por_longitud(contenido))
+        y=list(Funciones.contar_palabras_por_longitud(contenido).values())
+        cmap = mpl.colormaps['RdYlGn']
+        norm = colors.Normalize(vmin=min(y), vmax=max(y))
+        histogram_len = plt.bar(x, y, color=cmap(norm(y)))
+        for bar in histogram_len:
+            yval = bar.get_height()
+            plt.text(bar.get_x(), yval + .005, yval)
+        plt.title('Frecuencia de palabras por longitud', fontsize=30)
+        plt.xlabel('Longitud de palabras', fontsize=20)
+        plt.ylabel('Frecuencia', fontsize=20)
+        plt.tight_layout()
+        plt.show()  
+    elif opcion_herramienta == 5:
+        print(Funciones.contar_frecuencia_palabras(contenido))
+    elif opcion_herramienta == 6:
+      print("Saliendo del programa...")
+      break
+    bucle = int(input("Desea volver a utilizar el programa?\n1. Si\n2. No\n"))
+
+
+
+for idioma in idiomas:
+    palabras_frecuentes = get_most_frequent_words_without_stopwords(contenido, idioma)
+    frecuentes_por_idioma[idioma] = palabras_frecuentes
+frecuentes_por_idioma
+
